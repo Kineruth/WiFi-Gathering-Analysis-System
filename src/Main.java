8e1d7ad90872a89284099f99ba501ad44860d118
Kineruth <naharykin@gmail.com>
2017-12-20 22:25:13 +0200

    added some comments to the main class

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,42 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// put a specific path's directory to make CSV file
-		MergeCSVfiles t = new MergeCSVfiles("C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\Ex2\\data\\BM1\\Wifi_scans");
+		/*
+		 * Create a merged CSV file with CSV files exported from the app WiGLE:
+		 * Call function sortDirFiles in class MergeCSVfiles.
+		 * Put in its constructor a specific path to a directory with those CSV files.
+		 */
+		String diractory_Path = "C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\Ex2\\data\\BM1\\Wifi_scans";
+		MergeCSVfiles t = new MergeCSVfiles(diractory_Path);
 //		t.sortDirFiles();
 
 		
-		
-		// put a specific path to CSV file to make KML file 
-//		ConvertCSVToKML k =	new ConvertCSVToKML("C:\\Users\\admin\\git\\Task1\\Wigle Wifi App - Exports - Examples - 2017.12.14.00.04.53.csv");
+		/*
+		 * Create a KML file for Google Earth:
+		 * Call function createFile in class ConvertCSVToKML.
+		 * Put in its constructor a specific path to the merged CSV file.
+		 */
+		String csv_Path = "C:\\Users\\admin\\git\\Task1\\Wigle Wifi App - Exports - Examples - 2017.12.14.00.04.53.csv";
+		ConvertCSVToKML k =	new ConvertCSVToKML(csv_Path);
 //		k.createFile();
 
 		
-		//Create a CSV file with all strongest mac correct location
+		/*Run Algorithm 1 - Create a CSV file with all strongest mac correct location:
+		 * filePath = the merged CSV file's path.
+		 * number =  a number of samples to be taken for the calculation.
+		 * Call function macStrongestLocation in class ArrangeCSV.
+		 * 
+		 * Run Algorithm 2 - Create a CSV file with the user location for every sample:
+		 * filePath = the merged CSV file's path.
+		 * fileOutput = the merged CSV file's path without coordinates.
+		 *  number =  a number of samples to be taken for the calculation.
+		 *  Call function userLocation in class ArrangeCSV.
+		 */
 		ArrangeCSV a = new ArrangeCSV();
 		String filePath = "C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\testing\\Boaz_Files\\_comb_all_BM2_.csv";
 		String fileOutput="C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\testing\\Boaz_Files\\_comb_no_gps_ts1.csv";
-
 		int number =4;
+		
 //		a.macStrongestLocation(filePath, number); //algo 1
 		a.userLocation(filePath, fileOutput, number); //algo 2
 	}
