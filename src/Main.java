f74cfa3198fd00a2df45b610be672042f49d02d9
Kineruth <naharykin@gmail.com>
2017-12-20 22:32:01 +0200

    added some comments to the main class

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,54 @@ package src;
 
 /**
  * Date: 23-11-2017
- * This class is the main class ,to run the whole project. The user needs to enter
- * the path for a directory - for class MergeCSVFiles, and a path to an arranged CSV file - for class ConvertToKML
+ * This class is the main class ,to run the whole project. 
+ * The user needs to enter a path for a directory - for class MergeCSVFiles, and a path to an arranged CSV file - for class ConvertToKML
  * @author Kineret Ruth Nahary & Yakir Amar
  *
  */
 public class Main {
-
+/**
+ * 
+ * One: 
+ * Create a merged CSV file with CSV files exported from the app WiGLE:
+ * Call function sortDirFiles in class MergeCSVfiles.
+ * Put in its constructor a specific path to a directory with those CSV files.
+ * Two: 
+ *  Create a KML file for Google Earth:
+ * Call function createFile in class ConvertCSVToKML.
+ *  Put in its constructor a specific path to the merged CSV file.
+ *  Three: 
+ *  Run Algorithm 1 - Create a CSV file with all strongest mac correct location:
+ * filePath = the merged CSV file's path.
+ * number =  a number of samples to be taken for the calculation.
+ * Call function macStrongestLocation in class ArrangeCSV.
+ * Four:
+ * Run Algorithm 2 - Create a CSV file with the user location for every sample:
+ * filePath = the merged CSV file's path.
+ * fileOutput = the merged CSV file's path without coordinates.
+ *  number =  a number of samples to be taken for the calculation.
+ *  Call function userLocation in class ArrangeCSV.
+ */
 	public static void main(String[] args) {
 
-		/*
-		 * Create a merged CSV file with CSV files exported from the app WiGLE:
-		 * Call function sortDirFiles in class MergeCSVfiles.
-		 * Put in its constructor a specific path to a directory with those CSV files.
-		 */
+		/* ONE: */
+		
 		String diractory_Path = "C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\Ex2\\data\\BM1\\Wifi_scans";
 		MergeCSVfiles t = new MergeCSVfiles(diractory_Path);
 //		t.sortDirFiles();
 
 		
-		/*
-		 * Create a KML file for Google Earth:
-		 * Call function createFile in class ConvertCSVToKML.
-		 * Put in its constructor a specific path to the merged CSV file.
-		 */
+		
+		/* TWO: */
+	
 		String csv_Path = "C:\\Users\\admin\\git\\Task1\\Wigle Wifi App - Exports - Examples - 2017.12.14.00.04.53.csv";
 		ConvertCSVToKML k =	new ConvertCSVToKML(csv_Path);
 //		k.createFile();
 
 		
-		/*Run Algorithm 1 - Create a CSV file with all strongest mac correct location:
-		 * filePath = the merged CSV file's path.
-		 * number =  a number of samples to be taken for the calculation.
-		 * Call function macStrongestLocation in class ArrangeCSV.
-		 * 
-		 * Run Algorithm 2 - Create a CSV file with the user location for every sample:
-		 * filePath = the merged CSV file's path.
-		 * fileOutput = the merged CSV file's path without coordinates.
-		 *  number =  a number of samples to be taken for the calculation.
-		 *  Call function userLocation in class ArrangeCSV.
-		 */
+		
+		/* THREE & FOUR: */
+		
 		ArrangeCSV a = new ArrangeCSV();
 		String filePath = "C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\testing\\Boaz_Files\\_comb_all_BM2_.csv";
 		String fileOutput="C:\\Users\\admin\\Documents\\Computer Science\\Second Year\\First Sem\\OOP\\Tasks\\Task2\\testing\\Boaz_Files\\_comb_no_gps_ts1.csv";
